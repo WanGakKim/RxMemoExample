e81628fb34c7775a63145d727007b2020378b6e8
wangak kim <wangak.kim@holich.net>
2020-09-04 22:46:56 +0900

    add view model list

---
+++ RxMemoExample/ViewModel/MemoComposeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MemoComposeViewModel.swift
+//  RxMemoExample
+//
+//  Created by Gak_ee on 2020/09/04.
+//  Copyright © 2020 Gak_eee. All rights reserved.
+//
+
+import Foundation
+
+class MemoComposeViewModel {
+    
+}
