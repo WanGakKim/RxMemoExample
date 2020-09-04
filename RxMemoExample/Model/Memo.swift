cf2ffabfb3a9e9ea3c439c2bedc04cf275cd1097
wangak kim <wangak.kim@holich.net>
2020-09-04 22:07:00 +0900

    add Memo model

---
+++ RxMemoExample/Model/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  RxMemoExample
+//
+//  Created by Gak_ee on 2020/09/03.
+//  Copyright © 2020 Gak_eee. All rights reserved.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    //New instance Method
+    init(original: Memo, updatedContent: String){
+        self = original
+        self.content = updatedContent
+    }
+}
