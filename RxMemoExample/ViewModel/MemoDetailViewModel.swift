6ee08603ed8e2998c09c0f63f93bd502a518a10d
wangak kim <wangak.kim@holich.net>
2020-09-12 01:14:54 +0900

    implement MemoDetailViewModel

--- RxMemoExample/ViewModel/MemoDetailViewModel.swift
+++ RxMemoExample/ViewModel/MemoDetailViewModel.swift
@@ -7,7 +7,30 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel : CommonViewModel{
+    let memo: Memo
     
+    private var formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "Ko_kr")
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+    
+    var contents: BehaviorSubject<[String]> // 메모를 편집하고 나서 메모가 편집 후에 반영되어야 함. 옵저버블로 하면 안됨.
+    
+    init(memo: Memo, title: String, sceneCoordinator: SeceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content
+            formatter.string(from: memo.insertDate)
+        ])
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
