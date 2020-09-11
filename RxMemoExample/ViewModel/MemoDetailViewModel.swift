156dbe5061a17a5ebe4b1efd383851cc86c97e30
wangak kim <wangak.kim@holich.net>
2020-09-12 02:41:46 +0900

    implement memo delete

--- RxMemoExample/ViewModel/MemoDetailViewModel.swift
+++ RxMemoExample/ViewModel/MemoDetailViewModel.swift
@@ -61,4 +61,11 @@ class MemoDetailViewModel : CommonViewModel{
                 .asObservable().map{ _ in}
         }
     }
+    
+    func makeDeleteAction() -> CocoaAction {
+        return Action { input in
+            self.storage.delete(memo: self.memo)
+            return self.sceneCoordinator.close(animated: true).asObservable().map{ ß_ in }
+        }
+    }
 }
